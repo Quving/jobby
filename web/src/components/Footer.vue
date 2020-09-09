aab55e87e4df63fa1ca4aea3258b5ec5e2a44fc6
Vinh Ngu <vinh-ngu@hotmail.com>
2020-09-10 01:55:13 +0200

    Style footer.

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-row justify="center">
-    <div>
-      <div>Programmed by <span class="light-blue"> Quving</span></div>
-      <div>Project is open-source and can be found at <a target="_blank"
-                                                               href="https://github.com/Quving/jobby">GitHub!</a></div>
-    </div>
-  </v-row>
+  <v-footer fixed>
+    <v-row justify="center">
+      <div>
+        <div>Programmed by <span class="light-blue"> Quving</span></div>
+        <div>Project is open-source and can be found at <a target="_blank"
+                                                           href="https://github.com/Quving/jobby">GitHub!</a></div>
+      </div>
+    </v-row>
+  </v-footer>
 </template>
