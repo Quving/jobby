33c929c88c965ff762217a4c688d8eab3dc077ad
Vinh Ngu <vinh-ngu@hotmail.com>
2020-09-12 05:28:01 +0200

    Use standard headers.

--- web/src/views/AddHost.vue
+++ web/src/views/AddHost.vue
@@ -1,46 +1,46 @@
 <template>
-    <v-container>
-        <v-col>
-            <h2 class="mb-7">New Host</h2>
-            <v-text-field
-                outlined
-                counter=25
-                hint="Select a good host name!"
-                autofocus
-                v-model="hostName"
-                label="Host name">
-            </v-text-field>
-            <v-textarea
-                outlined
-                counter=100
-                v-model="hostDescription"
-                label="Host Description"
-                hint="Your later self will be happy about a good description.">
-            </v-textarea>
-            <v-btn
-                :loading="addBtnLoading"
-                :disabled="false"
-                color="success"
-                @click="addHost">
-                Add Host
-            </v-btn>
-        </v-col>
-    </v-container>
+  <v-container>
+    <h1 class="text-center">CREATE HOST</h1>
+    <v-col>
+      <v-text-field
+          outlined
+          counter=25
+          hint="Select a good host name!"
+          autofocus
+          v-model="hostName"
+          label="Host name">
+      </v-text-field>
+      <v-textarea
+          outlined
+          counter=100
+          v-model="hostDescription"
+          label="Host Description"
+          hint="Your later self will be happy about a good description.">
+      </v-textarea>
+      <v-btn
+          :loading="addBtnLoading"
+          :disabled="false"
+          color="success"
+          @click="addHost">
+        Add Host
+      </v-btn>
+    </v-col>
+  </v-container>
 </template>
 <script>
-    export default {
-        name: 'App',
-        components: {},
-        data: () => ({
-            addBtnLoading: false,
-            hostName: "",
-            hostDescription: "",
-        }),
-        methods: {
-            addHost: function () {
+export default {
+  name: 'App',
+  components: {},
+  data: () => ({
+    addBtnLoading: false,
+    hostName: "",
+    hostDescription: "",
+  }),
+  methods: {
+    addHost: function () {
 
-            }
-        }
-    };
+    }
+  }
+};
 </script>
 
--- web/src/views/AddHostGroup.vue
+++ web/src/views/AddHostGroup.vue
@@ -1,46 +1,46 @@
 <template>
-    <v-container>
-        <v-col>
-            <h2 class="mb-7">New HostGroup</h2>
-            <v-text-field
-                outlined
-                counter=25
-                hint="Select a good hostGroup name!"
-                autofocus
-                v-model="hostGroupName"
-                label="HostGroup name">
-            </v-text-field>
-            <v-textarea
-                outlined
-                counter=100
-                v-model="hostGroupDescription"
-                label="HostGroup Description"
-                hint="Your later self will be happy about a good description.">
-            </v-textarea>
-            <v-btn
-                :loading="addBtnLoading"
-                :disabled="false"
-                color="success"
-                @click="addHostGroup">
-                Add HostGroup
-            </v-btn>
-        </v-col>
-    </v-container>
+  <v-container>
+    <h1 class="text-center">CREATE HOSTGROUP</h1>
+    <v-col>
+      <v-text-field
+          outlined
+          counter=25
+          hint="Select a good hostGroup name!"
+          autofocus
+          v-model="hostGroupName"
+          label="HostGroup name">
+      </v-text-field>
+      <v-textarea
+          outlined
+          counter=100
+          v-model="hostGroupDescription"
+          label="HostGroup Description"
+          hint="Your later self will be happy about a good description.">
+      </v-textarea>
+      <v-btn
+          :loading="addBtnLoading"
+          :disabled="false"
+          color="success"
+          @click="addHostGroup">
+        Add HostGroup
+      </v-btn>
+    </v-col>
+  </v-container>
 </template>
 <script>
-    export default {
-        name: 'App',
-        components: {},
-        data: () => ({
-            addBtnLoading: false,
-            hostGroupName: "",
-            hostGroupDescription: "",
-        }),
-        methods: {
-            addHostGroup: function () {
+export default {
+  name: 'App',
+  components: {},
+  data: () => ({
+    addBtnLoading: false,
+    hostGroupName: "",
+    hostGroupDescription: "",
+  }),
+  methods: {
+    addHostGroup: function () {
 
-            }
-        }
-    };
+    }
+  }
+};
 </script>
 
