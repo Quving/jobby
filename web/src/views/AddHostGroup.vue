ae30fdc59162f57e6bbf1292619e9a878473fab6
Vinh Ngu <vinh-ngu@hotmail.com>
2020-09-23 14:53:37 +0200

    Implement form to add jobgroups and hostgroups.

--- web/src/views/AddHostGroup.vue
+++ web/src/views/AddHostGroup.vue
@@ -1,44 +1,81 @@
 <template>
   <v-container>
-    <h1 class="text-center">CREATE HOSTGROUP</h1>
-    <v-col>
-      <v-text-field
-          outlined
-          counter=25
-          hint="Select a good hostGroup name!"
-          autofocus
-          v-model="hostGroupName"
-          label="HostGroup name">
-      </v-text-field>
-      <v-textarea
-          outlined
-          counter=100
-          v-model="hostGroupDescription"
-          label="HostGroup Description"
-          hint="Your later self will be happy about a good description.">
-      </v-textarea>
-      <v-btn
-          :loading="addBtnLoading"
-          :disabled="false"
-          color="success"
-          @click="addHostGroup">
-        Add HostGroup
-      </v-btn>
-    </v-col>
+    <form @submit.prevent="submit" autocomplete="on">
+      <h1 class="text-center">CREATE HOSTGROUP</h1>
+      <v-col>
+        <v-text-field
+            required
+            outlined
+            counter=25
+            :rules="nameRule"
+            hint="Select a good hostGroup name!"
+            autofocus
+            v-model="hostGroupName"
+            label="HostGroup name">
+        </v-text-field>
+        <v-textarea
+            required
+            outlined
+            counter=100
+            :rules="descriptionRule"
+            v-model="hostGroupDescription"
+            label="HostGroup Description"
+            hint="Your later self will be happy about a good description.">
+        </v-textarea>
+        <v-alert class="mt-5 mb-5" dense v-if='status' v-bind:type="alert_type">{{ status }}</v-alert>
+        <v-btn
+            :loading="addBtnLoading"
+            :disabled="false"
+            type="submit"
+            color="success">
+          Add HostGroup
+        </v-btn>
+      </v-col>
+    </form>
   </v-container>
 </template>
 <script>
+import JobbyApi from "@/services/jobbyApi";
+
 export default {
   name: 'App',
   components: {},
   data: () => ({
+    nameRule: [
+      v => !!v || 'Setting a name is required.',
+      v => v.length <= 100 || 'Must be less than 100 characters.'
+    ],
+    descriptionRule: [
+      v => !!v || 'Providing a description is required.',
+      v => v.length <= 500 || 'Must be less than 500 characters.'
+    ],
     addBtnLoading: false,
     hostGroupName: "",
     hostGroupDescription: "",
+    alert_type: "",
+    status: "",
   }),
   methods: {
-    addHostGroup: function () {
+    submit: function () {
+      this.addBtnLoading = true;
+      const data = {
+        name: this.hostGroupName,
+        description: this.hostGroupDescription
+      };
 
+      JobbyApi.createHostGroup(data).then((response) => {
+        // ToDo: Parse reponse status code form more detailed informations.
+        response;
+        this.alert_type = 'success';
+        this.status = 'Host created.';
+      }, (error) => {
+        this.alert_type = 'error';
+        this.status = 'Host could not be created.';
+        for (const [key, value] of Object.entries(error)) {
+          this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
+        }
+      });
+      this.addBtnLoading = false;
     }
   }
 };
--- web/src/views/AddJobGroup.vue
+++ web/src/views/AddJobGroup.vue
@@ -1,46 +1,78 @@
 <template>
   <v-container>
-    <h1 class="text-center">CREATE JOBGROUP</h1>
-    <v-col>
-      <v-text-field
-          outlined
-          counter=25
-          hint="Select a good jobGroup name!"
-          autofocus
-          v-model="jobGroupName"
-          label="JobGroup name">
-      </v-text-field>
-      <v-textarea
-          outlined
-          counter=100
-          v-model="jobGroupDescription"
-          label="JobGroup Description"
-          hint="Your later self will be happy about a good description.">
-      </v-textarea>
-      <v-btn
-          :loading="addBtnLoading"
-          :disabled="false"
-          color="success"
-          @click="addJobGroup">
-        Add JobGroup
-      </v-btn>
-    </v-col>
+    <form @submit.prevent="submit" autocomplete="on">
+      <h1 class="text-center">CREATE JOBGROUP</h1>
+      <v-col>
+        <v-text-field
+            required
+            outlined
+            counter=25
+            :rules="nameRule"
+            hint="Select a good jobGroup name!"
+            autofocus
+            v-model="jobGroupName"
+            label="JobGroup name">
+        </v-text-field>
+        <v-textarea
+            required
+            outlined
+            counter=100
+            :rules="descriptionRule"
+            v-model="jobGroupDescription"
+            label="JobGroup Description"
+            hint="Your later self will be happy about a good description.">
+        </v-textarea>
+        <v-btn
+            :loading="addBtnLoading"
+            :disabled="false"
+            type="submit"
+            color="success">
+          Add JobGroup
+        </v-btn>
+      </v-col>
+    </form>
   </v-container>
 </template>
 <script>
+import JobbyApi from "@/services/jobbyApi";
+
 export default {
   name: 'App',
   components: {},
   data: () => ({
+    nameRule: [
+      v => !!v || 'Setting a name is required.',
+      v => v.length <= 100 || 'Must be less than 100 characters.'
+    ],
+    descriptionRule: [
+      v => !!v || 'Providing a description is required.',
+      v => v.length <= 500 || 'Must be less than 500 characters.'
+    ],
     addBtnLoading: false,
     jobGroupName: "",
     jobGroupDescription: "",
   }),
   methods: {
-    addJobGroup: function () {
+    submit: function () {
+      this.addBtnLoading = true;
+      const data = {
+        name: this.jobGroupName,
+        description: this.jobGroupDescription
+      };
 
+      JobbyApi.createJobGroup(data).then((response) => {
+        console.log(response);
+        this.alert_type = 'success';
+        this.status = 'Job created.';
+      }, (error) => {
+        this.alert_type = 'error';
+        this.status = 'Job could not be created.';
+        for (const [key, value] of Object.entries(error)) {
+          this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
+        }
+      });
+      this.addBtnLoading = false;
     }
   }
 };
 </script>
-
