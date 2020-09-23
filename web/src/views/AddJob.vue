d9fbab3dbf8dc573adb3c396a8eb6a91bda1f7ee
Vinh Ngu <vinh-ngu@hotmail.com>
2020-09-23 15:20:27 +0200

    Implement form to add job.

--- web/src/views/AddJob.vue
+++ web/src/views/AddJob.vue
@@ -1,69 +1,133 @@
 <template>
   <v-container>
     <h1 class="text-center">CREATE JOB</h1>
-    <v-col>
-      <v-text-field
-          outlined
-          counter=25
-          hint="Select a good job name!"
-          autofocus
-          v-model="jobName"
-          label="Job name">
-      </v-text-field>
-      <v-textarea
-          outlined
-          counter=100
-          v-model="jobDescription"
-          label="Job Description"
-          hint="Your later self will be happy about a good description.">
-      </v-textarea>
-      <v-select
-          outlined
-          v-model="selectedJobGroup"
-          :items="jobGroups"
-          :menu-props="{ maxHeight: '400', maxWidth:'200' }"
-          label="JobGroup"
-          hint="Select JobGroup"
-          persistent-hint
-      ></v-select>
-      <v-select
-          outlined
-          class="mt-7 mb-7"
-          v-model="selectedHostGroup"
-          :items="hostGroups"
-          :menu-props="{ maxHeight: '400', maxWidth:'200' }"
-          label="HostGroup"
-          hint="Select HostGroup"
-          persistent-hint
-      ></v-select>
-      <v-btn
-          :loading="addBtnLoading"
-          :disabled="false"
-          color="success"
-          @click="addJob">
-        Add Job
-      </v-btn>
-    </v-col>
+    <form @submit.prevent="submit" autocomplete="on">
+      <v-col>
+        <v-text-field
+            required
+            outlined
+            counter=25
+            hint="Select a good job name!"
+            autofocus
+            v-model="jobName"
+            label="Job name">
+        </v-text-field>
+        <v-textarea
+            required
+            outlined
+            counter=100
+            v-model="jobDescription"
+            label="Job Description"
+            hint="Your later self will be happy about a good description.">
+        </v-textarea>
+        <v-select
+            outlined
+            v-model="selectedJobGroup"
+            :items="jobGroups"
+            :menu-props="{ maxHeight: '400', maxWidth:'200' }"
+            item-text="name"
+            item-value="id"
+            label="JobGroup"
+            hint="Select JobGroup"
+            persistent-hint
+        ></v-select>
+        <v-select
+            outlined
+            class="mt-7 mb-7"
+            v-model="selectedHostGroup"
+            item-text="name"
+            item-value="id"
+            :items="hostGroups"
+            :menu-props="{ maxHeight: '400', maxWidth:'200' }"
+            label="HostGroup"
+            hint="Select HostGroup"
+            persistent-hint
+        ></v-select>
+        <v-alert class="mt-5 mb-5" dense v-if='status' v-bind:type="alert_type">{{ status }}</v-alert>
+        <v-btn
+            :loading="addBtnLoading"
+            :disabled="false"
+            color="success"
+            type="submit">
+          Add Job
+        </v-btn>
+      </v-col>
+    </form>
   </v-container>
 </template>
 <script>
+import JobbyApi from "@/services/jobbyApi";
+
 export default {
   name: 'App',
   components: {},
   data: () => ({
-    addBtnLoading: false,
+    // Status bar
+    alert_type: "",
+    status: "",
+
+    // Form variables
+    nameRule: [
+      v => !!v || 'Setting a name is required.',
+      v => v.length <= 100 || 'Must be less than 100 characters.'
+    ],
+    descriptionRule: [
+      v => !!v || 'Providing a description is required.',
+      v => v.length <= 500 || 'Must be less than 500 characters.'
+    ],
+    osRule: [
+      v => !!v || 'Providing a OS is required.',
+      v => v.length <= 25 || 'Must be less than 25 characters.'
+    ],
     jobName: "",
     jobDescription: "",
-    selectedJobGroup: "",
-    selectedHostGroup: "",
-    jobGroups: ["Job Group 1", "Job Group 2"],
-    hostGroups: ["Host Group 1", "Host Group 2"],
-  }),
-  methods: {
-    addJob: function () {
+    selectedJobGroup: 0,
+    selectedHostGroup: 0,
+    jobGroups: [],
+    hostGroups: [],
 
+    addBtnLoading: false,
+  }),
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function () {
+      JobbyApi.listHosts().then(response => {
+        this.hostGroups = response;
+      }, (error => {
+        console.log(error)
+      }));
+      JobbyApi.listJobGroups().then(response => {
+        this.jobGroups = response;
+      }, (error => {
+        console.log(error)
+      }));
+    },
+    submit: function () {
+      this.addBtnLoading = true;
+
+      const data = {
+        name: this.jobName,
+        jobgroup: this.selectedJobGroup,
+        host: this.selectedHostGroup,
+        description: this.jobDescription
+      };
+
+      JobbyApi.createJob(data).then((response) => {
+        response;
+        this.alert_type = 'success';
+        this.status = 'Host created.';
+      }, (error) => {
+        this.alert_type = 'error';
+        this.status = 'Job could not be created.';
+        for (const [key, value] of Object.entries(error)) {
+          this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
+        }
+      });
+      this.addBtnLoading = false;
     }
   }
-};
+}
 </script>
 
