38addafb312a1bf2683d339eec03723b3c69fa36
Vinh Ngu <vinh-ngu@hotmail.com>
2020-10-05 18:06:45 +0200

    Implement crud-operations for 'Jobgroup' on front-end.

--- web/src/views/CrudJobGroup.vue
+++ web/src/views/CrudJobGroup.vue
@@ -1,9 +1,10 @@
 <template>
   <v-container>
-    <view-headers header="Create Jobgroup"></view-headers>
+    <view-headers :header="headerText"></view-headers>
     <form @submit.prevent="submit" autocomplete="on">
       <v-col>
         <v-text-field
+            :readonly="formReadOnly"
             required
             outlined
             counter=25
@@ -12,6 +13,7 @@
             label="JobGroup name">
         </v-text-field>
         <v-textarea
+            :readonly="formReadOnly"
             required
             outlined
             counter=100
@@ -19,17 +21,20 @@
             label="JobGroup Description"
             hint="Your later self will be happy about a good description.">
         </v-textarea>
+        <v-alert class="mt-5 mb-5" dense v-if='status' v-bind:type="alert_type">{{ status }}</v-alert>
         <v-btn
-            :loading="addBtnLoading"
+            v-if="action !== 'read'"
+            :loading="submitBtnLoading"
             :disabled="false"
             type="submit"
-            color="success">
-          Add JobGroup
+            :color="submitBtnColor">
+          {{ submitBtnText }}
         </v-btn>
       </v-col>
     </form>
   </v-container>
 </template>
+
 <script>
 import JobbyApi from "@/services/jobbyApi";
 import ViewHeaders from "@/components/ViewHeader";
@@ -37,31 +42,145 @@ import ViewHeaders from "@/components/ViewHeader";
 export default {
   name: 'App',
   components: {ViewHeaders},
-  data: () => ({
-    addBtnLoading: false,
-    jobGroupName: "",
-    jobGroupDescription: "",
-  }),
+  data() {
+    return {
+      // Other
+      id: this.$route.params.id,
+      action: this.$route.params.action,
+
+      // Form
+      submitBtnLoading: false,
+      formReadOnly: true,
+      submitBtnColor: "",
+      submitBtnText: "",
+      jobGroupName: "",
+      jobGroupDescription: "",
+
+      // Site
+      headerText: "",
+
+      // Status Bar
+      alert_type: "",
+      status: "",
+    }
+  },
+  created() {
+    this.initState();
+  },
   methods: {
-    submit: function () {
-      this.addBtnLoading = true;
+    initState: function () {
+      const dynamicActionVars = {
+        create: {
+          fetchObjectOfInterest: false,
+          formReadOnly: false,
+          submitBtnColor: "success",
+          headerText: "Add JobGroup",
+          submitBtnText: "Add JobGroup"
+        },
+        read: {
+          fetchObjectOfInterest: true,
+          formReadOnly: true,
+          submitBtnColor: "warning",
+          headerText: "View JobGroup",
+          submitBtnText: "Update"
+        },
+        update: {
+          fetchObjectOfInterest: true,
+          formReadOnly: false,
+          submitBtnColor: "warning",
+          headerText: "Update JobGroup",
+          submitBtnText: "Update"
+        },
+        delete: {
+          fetchObjectOfInterest: true,
+          formReadOnly: true,
+          submitBtnColor: "error",
+          headerText: "Delete JobGroup",
+          submitBtnText: "Delete"
+        },
+      };
+      const dynamicVariables = dynamicActionVars[this.action];
+      this.formReadOnly = dynamicVariables.formReadOnly;
+      this.submitBtnText = dynamicVariables.submitBtnText;
+      this.headerText = dynamicVariables.headerText;
+      this.submitBtnColor = dynamicVariables.submitBtnColor;
+
+      if (dynamicVariables.fetchObjectOfInterest) {
+        JobbyApi.getJobGroup(this.id).then(data => {
+          this.jobGroupName = data.name;
+          this.jobGroupDescription = data.description;
+        });
+      }
+    },
+    createJobGroup: function () {
+      this.submitBtnLoading = true;
       const data = {
         name: this.jobGroupName,
         description: this.jobGroupDescription
       };
 
+      // eslint-disable-next-line no-unused-vars
       JobbyApi.createJobGroup(data).then((response) => {
-        console.log(response);
         this.alert_type = 'success';
-        this.status = 'Job created.';
+        this.status = 'JobGroup created.';
+        this.submitBtnLoading = false;
       }, (error) => {
         this.alert_type = 'error';
-        this.status = 'Job could not be created.';
+        this.status = 'JobGroup could not be created.';
         for (const [key, value] of Object.entries(error)) {
           this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
         }
+        this.submitBtnLoading = false;
       });
-      this.addBtnLoading = false;
+    },
+    updateJobGroup: function () {
+      this.submitBtnLoading = true;
+      const data = {
+        name: this.jobGroupName,
+        description: this.jobGroupDescription
+      };
+
+      // eslint-disable-next-line no-unused-vars
+      JobbyApi.updateJobGroup(this.id, data).then((response) => {
+        this.alert_type = 'success';
+        this.status = 'JobGroup updated.';
+        this.submitBtnLoading = false;
+      }, (error) => {
+        this.alert_type = 'error';
+        this.status = 'JobGroup could not be updated.';
+        for (const [key, value] of Object.entries(error)) {
+          this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
+        }
+        this.submitBtnLoading = false;
+      });
+    },
+    deleteJobGroup: function () {
+      this.submitBtnLoading = true;
+      // eslint-disable-next-line no-unused-vars
+      JobbyApi.deleteJobGroup(this.id).then((response) => {
+        this.alert_type = 'success';
+        this.status = 'JobGroup deleted. You will be redirected back in a few seconds.';
+        this.submitBtnLoading = false;
+      }, (error) => {
+        console.log(error);
+        this.alert_type = 'error';
+        this.status = 'JobGroup could not be deleted.';
+        for (const [key, value] of Object.entries(error)) {
+          this.status += `${status}\n ${key.toUpperCase()}: ${value}`;
+        }
+        this.submitBtnLoading = false;
+      });
+    },
+    submit: function () {
+      const actionFunctions = {
+        create: this.createJobGroup,
+        update: this.updateJobGroup,
+        delete: this.deleteJobGroup,
+      };
+      actionFunctions[this.action]();
+      setTimeout(() => {
+        this.$router.push("/jobs");
+      }, 1000);
     }
   }
 };
