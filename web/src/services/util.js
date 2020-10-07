e8586d0442322fa0dad17d35e2f8cfbebbd97d9a
Vinh Ngu <vinh-ngu@hotmail.com>
2020-10-08 00:03:48 +0200

    Set status 'success' in generated snippets and set name.

--- web/src/services/util.js
+++ web/src/services/util.js
@@ -3,7 +3,7 @@ export default {
         const config = require('../config');
         const authHeader = `-H 'Authorization: Token ${token}'`;
         const headers = "-H 'Content-type: application/json' -H 'accept: application/json'";
-        const body = `-d '{"job": ${jobId}, "status": "STATUS", "logs": "LOGS", "name": "NAME"}'`;
+        const body = `-d '{"job": ${jobId}, "status": "success", "logs": "LOGS", "name": "report_${jobId}"}'`;
         const url = `${config.envs.apiHostUrl}/resources/report/`;
         return `curl -X POST \\\n\t${authHeader} \\\n\t${headers} \\\n\t${body} \\\n\t${url}`;
     },
@@ -11,7 +11,7 @@ export default {
         let cmd = [
             "import requests",
             `headers = {\n\t'Authorization': 'Token ${token}',\n\t'Content-type': 'application/json', \n\t'accept': 'application/json',\n}`,
-            `data = '{"job": ${jobId}, "status": "STATUS", "logs": "LOGS", "name": "NAME"}'`,
+            `data = '{"job": ${jobId}, "status": "success", "logs": "LOGS", "name": "report_${jobId}"}'`,
             `response = requests.post('http://localhost:8000/resources/report/', headers=headers, data=data)`];
         return cmd.join('\n');
     }
