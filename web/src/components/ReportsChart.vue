2b68e9e2975ed520260a7582bf13e2a0b7a43367
Vinh Ngu <vinh-ngu@hotmail.com>
2020-12-26 05:59:46 +0100

    Implement report history chart.

--- web/src/components/ReportsChart.vue
+++ web/src/components/ReportsChart.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container>
-    <v-card>
+    <v-card :loading="isFetching">
       <v-card-title>
         {{ title }}
       </v-card-title>
       <line-chart
+          :key="datasets.toString()"
           :title="title"
           :datasets="datasets"
           :labels="labels">
@@ -15,6 +16,8 @@
 <script>
 import {Line} from 'vue-chartjs'
 import Vue from "vue";
+import util from "@/services/util";
+import jobbyApi from "@/services/jobbyApi";
 
 Vue.component('line-chart', {
   extends: Line,
@@ -31,19 +34,48 @@ Vue.component('line-chart', {
   }
 })
 
+
 export default {
   data() {
     return {
+      pageSizeReport: 1000,
+
+      // Status
+      isFetching: false,
+
+      // Chart
       title: "Reports History",
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 39, 10, 40, 39, 80, 40]
-        }
-      ]
+      labels: [],
+      datasets: [],
     }
   },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function () {
+      this.isFetching = true;
+      let last30Days = util.getAllDatesBetweenDates(
+          new Date(new Date().setDate(new Date().getDate() - 29)),
+          new Date()
+      );
+      this.labels = last30Days.map((value) => value.toISOString().split('T')[0]);
+      let dataPoints = []
+      for (let i = 0; i < this.labels.length; i++) {
+        const date = this.labels[i];
+        const urlParams = `?limit=100&createdAt=${date}`;
+        const data = await jobbyApi.listReports(urlParams);
+        dataPoints.push(data.count);
+      }
+      this.datasets = [
+        {
+          label: 'Data One',
+          backgroundColor: 'darkcyan',
+          data: dataPoints
+        }
+      ];
+      this.isFetching = false;
+    }
+  }
 }
 </script>
--- web/src/services/util.js
+++ web/src/services/util.js
@@ -39,5 +39,21 @@ export default {
             }
         })
         return objects;
-    }
+    },
+
+    // Returns an array of dates between the two dates
+    getAllDatesBetweenDates: function (startDate, endDate) {
+        var dates = [],
+            currentDate = startDate,
+            addDays = function (days) {
+                var date = new Date(this.valueOf());
+                date.setDate(date.getDate() + days);
+                return date;
+            };
+        while (currentDate <= endDate) {
+            dates.push(currentDate);
+            currentDate = addDays.call(currentDate, 1);
+        }
+        return dates;
+    },
 }
